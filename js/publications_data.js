31db7d886757f02370197ebdd5f509f0d9260050
kimmyju <yuido20547@naver.com>
2026-03-09 16:21:22 +0900

    changed image name

--- js/publications_data.js
+++ js/publications_data.js
@@ -109,7 +109,7 @@ const PUBLICATIONS_DATA_LOCAL = [
         "links": {
             "conf": "https://iccv.thecvf.com/"
                  },
-        "img": "/img/Publications/2026_ICCVW_Juhun.png",
+        "img": "/img/Publications/2025_ICCVW_Juhun.png",
         "abstract": "Image diffusion models are trained on independently sampled static images. While this is the bedrock task protocol in generative modeling, capturing the temporal world through the lens of static snapshots is information-deficient by design. This limitation leads to slower convergence, limited distributional coverage, and reduced generalization. In this work, we propose a simple and effective training strategy that leverages the temporal inductive bias present in continuous video frames to improve diffusion training. Notably, the proposed method requires no architectural modification and can be seamlessly integrated into standard diffusion training pipelines. We evaluate our method on the HandCo dataset, where hand-object interactions exhibit dense temporal coherence andsubtle variations in finger articulation often result in semantically distinct motions. Empirically, our method accelerates convergence by over 2x faster and achieves lower FID on both training and validation distributions. It also improves generative diversity by encouraging the model to capture meaningful temporal variations. We further provide an optimization analysis showing that our regularization reduces the gradient variance, which contributes to faster convergence."
     },
     {

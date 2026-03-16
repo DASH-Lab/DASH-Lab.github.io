351ed8c36c9cc9773b185362bd43dd50fdc0aa33
kimmyju <yuido20547@naver.com>
2026-03-16 10:45:15 +0900

    Updated error

--- js/membersdata.js
+++ js/membersdata.js
@@ -1,9 +1,9 @@
 // --- Members Data ---
 // Load this in your HTML before members.js
 
-// const researchProfessors = [
-//     {name: "Binh M. Le", role: "Research Professor", dept: "Computer Science & Engineering", interests: "Adversarial Robustness, Representation Learning",email: "bmle@g.skku.edu", img: "img/member_images/BinhMLe.jpg", link: "https://sites.google.com/view/binhminhle/home"}
-// ];
+const researchProfessors = [
+    //{name: "Binh M. Le", role: "Research Professor", dept: "Computer Science & Engineering", interests: "Adversarial Robustness, Representation Learning",email: "bmle@g.skku.edu", img: "img/member_images/BinhMLe.jpg", link: "https://sites.google.com/view/binhminhle/home"}
+];
 
 const phdStudents = [
     { name: "Eunju Park", dept: "Computer Science & Engineering", interests: "Deepfakes", email: "cindi01@g.skku.edu", img: "img/member_images/EunjuPark.jpg" },

7461bdb874ed9ee2e15de552da3e6c6ce4ccab82
Kimmyju <yuido20547@naver.com>
2026-06-30 18:28:59 +0900

    Added 2 News articles

--- _site/js/newsdata.js
+++ _site/js/newsdata.js
@@ -3,6 +3,8 @@
 // It must be loaded before news.js in your HTML.
 
 const newsData = [
+    { icon: '▶️', date: 'Jun 2026', year: 2026, text: 'KBS News: <b>[남북의 창] AI가 되살린 전쟁영웅…나라에 바친 청춘 ‘그대로’ [통일로 미래로]</b> [<a href="https://www.youtube.com/watch?v=HIK7o6ebyVU" target="_blank">media</a>]' },
+    { icon: '📰', date: 'Jun 2026', year: 2026, text: ' TV조선: <b>6·25 참전용사 사진, AI로 복원…유족 "살아 돌아오신듯"</b> [<a href="https://news.tvchosun.com/site/data/html_dir/2026/06/25/2026062590040.html" target="_blank">media</a>]' },
     { icon: '📰', date: 'Apr 2026', year: 2026, text: '동아일보: <b>“기억을 지웁니다”…AI ‘무한 학습’ 시대 가고 ‘망각’의 시대 오나</b> [<a href="https://www.donga.com/news/amp/all/20260427/133819471/1" target="_blank">media</a>]' },
     { icon: '📄', date: 'Apr 2026', year: 2026, text: 'One paper accepted at Demo & Challenge track of <b>ICMR 2026</b> ' },
     { icon: '📄', date: 'Feb 2026', year: 2026, text: 'Two papers accepted at findings track of <b>CVPR 2026 (BK IF=N/A)</b> ' },
